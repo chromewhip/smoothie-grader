2c4135add3ed42cc95affa45666e7de558cbb543
Connor <connorsteele604@gmail.com>
2016-03-06 19:36:59 -0800

    updated foodcsv database

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20160225203944) do
   enable_extension "plpgsql"
 
   create_table "foods", force: true do |t|
-    t.string "name"
+    t.string "name",            null: false
     t.float  "water"
     t.float  "energy"
     t.float  "protein"
